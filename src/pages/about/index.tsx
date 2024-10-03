97b44787e0b5fd1ead0dc992ccefec7cf8e8ab0c
YujiAdachi <adachi389@mec-np.com>
2024-10-03 14:38:03 +0900

    first commit

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Amplify SSG </h1>
+      <p>Sub page</p>
+      <a href='/'>Go to Home</a>
+    </div>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Amplify SSG </h1>
+      <p>Main page</p>
+      <a href='/about'>Go to About</a>
+    </div>
+  );
+}
